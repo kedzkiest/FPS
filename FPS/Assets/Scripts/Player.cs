3bacb759b292a7bcbe6020eae7dd87e2e68d9987
kedzkiest <kaede12kazu22@gmail.com>
2024-09-22 21:23:00 +0900

    クラス名を変更

--- FPS/Assets/Scripts/Player.cs
+++ FPS/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 
-public class PlayerCharacterController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] bool isAlive;
     public bool IsAlive
